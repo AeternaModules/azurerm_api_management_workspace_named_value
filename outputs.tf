d4a69d0dd6af6e73b19669357bf48dc3a23a44a6
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "api_management_workspace_named_values_id" {
   description = "Map of id values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.id }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_workspace_named_values_api_management_workspace_id" {
   description = "Map of api_management_workspace_id values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.api_management_workspace_id }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.api_management_workspace_id if v.api_management_workspace_id != null && length(v.api_management_workspace_id) > 0 }
 }
 output "api_management_workspace_named_values_display_name" {
   description = "Map of display_name values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.display_name }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.display_name if v.display_name != null && length(v.display_name) > 0 }
 }
 output "api_management_workspace_named_values_name" {
   description = "Map of name values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.name }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "api_management_workspace_named_values_secret" {
   description = "Map of secret values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.secret }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.secret if v.secret != null }
 }
 output "api_management_workspace_named_values_tags" {
   description = "Map of tags values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.tags }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 output "api_management_workspace_named_values_value" {
   description = "Map of value values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.value }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.value if v.value != null && length(v.value) > 0 }
   sensitive   = true
 }
 output "api_management_workspace_named_values_value_from_key_vault" {
   description = "Map of value_from_key_vault values across all api_management_workspace_named_values, keyed the same as var.api_management_workspace_named_values"
-  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.value_from_key_vault }
+  value       = { for k, v in azurerm_api_management_workspace_named_value.api_management_workspace_named_values : k => v.value_from_key_vault if v.value_from_key_vault != null && length(v.value_from_key_vault) > 0 }
 }
 
